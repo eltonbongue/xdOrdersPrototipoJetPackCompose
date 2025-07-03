dc7a14331fca867c259b32661c86c61edea714fa
Elton <eltonbongue@gmail.com>
2025-07-03 17:45:03 +0100

    add Setting screen

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/start_activity.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/start_activity.kt
@@ -1,2 +1,1 @@
 package com.elton.xdordersprototipojetpackcompose.ui.screens
-
--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/user_login.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/user_login.kt
@@ -1,2 +1,22 @@
 package com.elton.xdordersprototipojetpackcompose.ui.screens
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.elton.xdordersprototipojetpackcompose.ui.components.TopBarXD
+
+
+@Composable
+fun UserLoginScreen(navController: NavController) {
+
+    Scaffold(
+        topBar = {
+            TopBarXD(
+                title = "Usuários",
+                navController = navController
+            )
+        }
+    )
+    { _ -> }
+
+    }
