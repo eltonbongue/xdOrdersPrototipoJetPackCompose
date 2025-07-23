803b7b0153b28acba4fe0b3101010151b3b6408c
Elton <eltonbongue@gmail.com>
2025-07-23 12:21:07 +0100

    add MessagePageScreen composable

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/Message/MessagePage.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/Message/MessagePage.kt
@@ -1,2 +1,41 @@
 package com.elton.xdordersprototipojetpackcompose.ui.screens.Message
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.elton.xdordersprototipojetpackcompose.components.TopBarOrderXD
+
+@Composable
+fun MessagePageScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopBarOrderXD(
+                title = "Mensagens",
+                navController = navController,
+
+                )
+        },
+
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .background(
+                        color = Color(0xFFF5F5F5),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+            ) {
+
+            }
+        }
+    )
+}
