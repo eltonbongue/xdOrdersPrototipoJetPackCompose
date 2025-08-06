c96a72ec13c7121debca892d33fcbb8597517603
Elton <eltonbongue@gmail.com>
2025-08-06 15:43:52 +0100

    add TablesViewModelFactory file

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/viewModel/TablesViewModelFactory.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/viewModel/TablesViewModelFactory.kt
@@ -1,4 +1,14 @@
 package com.elton.xdordersprototipojetpackcompose.viewModel
 
-class TablesViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.elton.xdordersprototipojetpackcompose.data.local.DAO
+
+class TablesViewModelFactory(private val dao: DAO) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TablesViewModel::class.java)) {
+            return TablesViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
