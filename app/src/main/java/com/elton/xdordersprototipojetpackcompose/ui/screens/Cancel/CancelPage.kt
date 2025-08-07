224d9e6bfaf63f730451d2e90b98879adfaec24a
Elton <eltonbongue@gmail.com>
2025-08-07 09:44:52 +0100

    refactor HorizontalPagerXD function

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/Cancel/CancelPage.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/Cancel/CancelPage.kt
@@ -7,18 +7,36 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.elton.xdordersprototipojetpackcompose.components.BaseOrderLayout
 import com.elton.xdordersprototipojetpackcompose.components.HorizontalPagerXD
+import com.elton.xdordersprototipojetpackcompose.data.local.DAO
+import com.elton.xdordersprototipojetpackcompose.data.local.DatabaseHelper
 import com.elton.xdordersprototipojetpackcompose.domain.model.Table
 import com.elton.xdordersprototipojetpackcompose.navigation.Screen
+import com.elton.xdordersprototipojetpackcompose.viewModel.TablesViewModel
+import com.elton.xdordersprototipojetpackcompose.viewModel.TablesViewModelFactory
 
 @Composable
 fun CancelPageScreen(navController: NavController) {
     val mesas = listOf<Table>()
+
+    val context = LocalContext.current
+
+    // Cria o DAO e o ViewModel manualmente
+    val dao = remember { DAO(DatabaseHelper(context)) }
+    val viewModel: TablesViewModel = viewModel(factory = TablesViewModelFactory(dao))
+
+    // Observa a lista de mesas
+    val mesasState = viewModel.mesas.collectAsState()
+
     Scaffold(
         topBar = {
             BaseOrderLayout(
@@ -29,8 +47,8 @@ fun CancelPageScreen(navController: NavController) {
                 HorizontalPagerXD(
                     navController = navController,
                     mesas = mesas,
-                    onMinhasButtonClick = { Table ->
-                        navController.navigate("cancel_page/${Table.id}")
+                    onMinhasButtonClick  = { table ->
+                        navController.navigate(Screen.CancelPagePrincipal.route)
                     }
                 )
 
