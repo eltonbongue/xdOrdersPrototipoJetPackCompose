db0d2a4c2ace58ff771247447a24830420a84ba4
Elton <eltonbongue@gmail.com>
2025-08-08 11:33:03 +0100

    refactor Minhaspage

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/components/pages/MinhasPage.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/components/pages/MinhasPage.kt
@@ -9,8 +9,9 @@ import androidx.compose.ui.unit.dp
 import com.elton.xdordersprototipojetpackcompose.domain.model.Table
 import com.elton.xdordersprototipojetpackcompose.ui.screens.Tables.MesaItem
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 
 @Composable
 fun MinhasScreen(
@@ -22,8 +23,11 @@ fun MinhasScreen(
             .fillMaxWidth()
             .padding(10.dp)
     ) {
-        LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier.fillMaxWidth()
         ) {
             items(mesas) { mesa ->
                 MesaItem(
