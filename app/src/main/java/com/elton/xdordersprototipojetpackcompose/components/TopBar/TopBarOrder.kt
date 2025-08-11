7780763b1f8e3681b59b1f97a6e64c34ace18d39
Elton <eltonbongue@gmail.com>
2025-08-11 16:22:24 +0100

    improve

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/components/TopBar/TopBarOrder.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/components/TopBar/TopBarOrder.kt
@@ -1,6 +1,5 @@
 package com.elton.xdordersprototipojetpackcompose.components.TopBar
 
-import android.R.attr.onClick
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
@@ -18,7 +17,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
