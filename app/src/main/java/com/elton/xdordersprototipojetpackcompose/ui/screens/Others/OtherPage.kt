2eebda2c7635f60263e3b2461f863dc38c180b5b
Elton <eltonbongue@gmail.com>
2025-08-18 16:13:05 +0100

    improve OtherPageScreen

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/Others/OtherPage.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/ui/screens/Others/OtherPage.kt
@@ -3,12 +3,15 @@ package com.elton.xdordersprototipojetpackcompose.ui.screens.Others
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -34,6 +37,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -45,11 +49,11 @@ import com.elton.xdordersprototipojetpackcompose.navigation.Screen.HomePage
 import com.elton.xdordersprototipojetpackcompose.ui.screens.Home.TopActionButton
 
 
+
 @Composable
 fun OtherPageScreen(navController: NavController, user: User) {
     val backgroundColor = Color(0xFFF5F5F5)
-    val buttonColor = Color(0xFFFFFFFF)
-    val fullButtonColor= Color(0xFF5E608D)
+    val buttonColor = Color(0xFF2C80AF)
 
     Column(
         modifier = Modifier
@@ -58,60 +62,89 @@ fun OtherPageScreen(navController: NavController, user: User) {
             .padding(16.dp)
     ) {
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Spacer( modifier = Modifier.height(16.dp))
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+
+
+        ){
+
+            Icon(
+                painter = painterResource(id = R.drawable.xd_logo),
+                contentDescription = "Logo XD",
+                modifier = Modifier.size(38.dp),
+                tint = Color(0xFF2C80AF)
+            )
+
+            Spacer(modifier = Modifier.width(32.dp))
+
+            Image(
+                painter = painterResource(R.drawable.account_user_png_photo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xFF2C80AF),
+                        shape = RoundedCornerShape(50.dp)
+                    )
+            )
+
+
+        }
+
+        Spacer( modifier = Modifier.height(16.dp))
+
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 40.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .background(buttonColor)
-                .padding(16.dp)
+                .height(150.dp)
+                .background(
+                    color = Color(0xFF2C80AF),
+                    shape = RoundedCornerShape(12.dp)
+                ),
+            contentAlignment = Alignment.Center
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth()
 
-            ) {
-                Column(
-                    modifier = Modifier,
-                    horizontalAlignment = Alignment.CenterHorizontally
 
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.account_user_png_photo),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(80.dp)
-                            .clip(RoundedCornerShape(8.dp))
-                            .background(Color.White)
-                    )
+            Text(
+                text = user.name,
+                color = Color.White,
+                fontSize = 20 .sp,
+                fontWeight = FontWeight.Bold,
+            )
 
-                    Text(
-                        text = user.name,
-                        color = Color(0xFF5E608D),
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(top = 8.dp)
-                    )
-                }
 
-                Spacer(modifier = Modifier.width(100.dp))
-
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.width(180.dp)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(100.dp)
+                    .offset(y = 75.dp) // empurra para baixo
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            )
+            {
+                Row(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(24.dp)),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     TopActionButton(
                         icon = Icons.Default.Upload,
                         label = "CAIXA DE SAÍDA",
                         onClick = { navController.navigate("outbox_page") }
                     )
-                    Divider(
-                        color = fullButtonColor.copy(alpha = 0.5f),
-                        thickness = 0.5.dp,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 16.dp)
+                    Spacer(
+                        modifier = Modifier.width(24.dp)
                     )
                     TopActionButton(
                         icon = Icons.Default.Message,
@@ -121,8 +154,10 @@ fun OtherPageScreen(navController: NavController, user: User) {
                 }
             }
         }
-            Column {
 
+        Spacer(modifier = Modifier.height(100.dp))
+
+            Column {
 
                     Spacer(modifier = Modifier.height(8.dp))
 
@@ -173,7 +208,6 @@ fun OtherPageScreen(navController: NavController, user: User) {
 
 
 
-
 @Composable
 fun FullWidthButtonOther(text: String, icon: ImageVector, backgroundColor: Color, onClick: () -> Unit) {
     androidx.compose.material3.Button(
@@ -181,17 +215,12 @@ fun FullWidthButtonOther(text: String, icon: ImageVector, backgroundColor: Color
         colors = ButtonDefaults.buttonColors(containerColor = backgroundColor),
         modifier = Modifier
             .fillMaxWidth()
-            .height(56.dp)
-            .border(
-                width = .5.dp,
-                color = Color(0xFF5E608D),
-                shape = RoundedCornerShape(12.dp)
-            ),
+            .height(56.dp),
         shape = RoundedCornerShape(8.dp)
     ) {
-        Icon(imageVector = icon, contentDescription = text, tint = Color.Black)
+        Icon(imageVector = icon, contentDescription = text, tint = Color.White)
         Spacer(modifier = Modifier.width(12.dp))
-        Text(text = text, color = Color(0xFF5E608D), fontSize = 14.sp, fontWeight = FontWeight.Bold)
+        Text(text = text, color = Color.White, fontSize = 14.sp, fontWeight = FontWeight.Bold)
     }
 }
 
