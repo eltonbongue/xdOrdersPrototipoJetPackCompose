2cb8f0a2ad4f222504620fcef30df90782053f2b
Elton <eltonbongue@gmail.com>
2025-08-18 16:31:32 +0100

    add PedidoComletoDto model

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/domain/model/PedidoCompletoDto.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/domain/model/PedidoCompletoDto.kt
@@ -1,4 +1,11 @@
 package com.elton.xdordersprototipojetpackcompose.domain.model
 
-class PedidoCompletoDto {
-}
+data class PedidoCompletoDto(
+    val orderId: Int,
+    val tableId: Int,
+    val productName: String,
+    val quantity: Int,
+    val unitPrice: Double,
+    val totalPrice: Double,
+    val complement: String?
+)
