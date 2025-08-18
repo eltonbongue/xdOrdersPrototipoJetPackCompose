8bee23d46a744ea9d71aa955b2c8cef9cab3f130
Elton <eltonbongue@gmail.com>
2025-08-18 16:01:47 +0100

    refactor ProdutoViewModel

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/viewModel/ProdutoViewModel.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/viewModel/ProdutoViewModel.kt
@@ -2,8 +2,11 @@ import androidx.compose.runtime.mutableStateMapOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.elton.xdordersprototipojetpackcompose.data.local.DAO.ProdutoDao
+import com.elton.xdordersprototipojetpackcompose.domain.model.OrderItem
 import com.elton.xdordersprototipojetpackcompose.domain.model.Product
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -11,6 +14,11 @@ class ProdutoViewModel(private val dao: ProdutoDao) : ViewModel() {
 
     private val _produtosPorCategoria = mutableStateMapOf<Int, List<Product>>()
     val produtosPorCategoria: Map<Int, List<Product>> get() = _produtosPorCategoria
+
+    // Estado: carrinho/pedido atual
+    private val _pedidoAtual = MutableStateFlow<List<OrderItem>>(emptyList())
+    val pedidoAtual: StateFlow<List<OrderItem>> = _pedidoAtual
+
 
     fun carregarProdutosPorCategoria(categoriaId: Int) {
         if (_produtosPorCategoria.containsKey(categoriaId)) return // evita chamadas desnecessárias
@@ -22,4 +30,7 @@ class ProdutoViewModel(private val dao: ProdutoDao) : ViewModel() {
             }
         }
     }
+
+
+
 }
