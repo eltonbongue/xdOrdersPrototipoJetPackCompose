5530f5aa6f10760a4b64aed9b7433fe8979f20d9
Elton <eltonbongue@gmail.com>
2025-08-20 11:18:09 +0100

    add properties of class XdordersApp

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/XdOrderApp.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/XdOrderApp.kt
@@ -1,4 +1,7 @@
 package com.elton.xdordersprototipojetpackcompose
 
-class XdOrderApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class XdOrderApp : Application()
