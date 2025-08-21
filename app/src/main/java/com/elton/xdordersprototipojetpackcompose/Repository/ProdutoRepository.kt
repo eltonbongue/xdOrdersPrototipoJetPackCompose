f02c1a23e43951da078ab834ea1fee52e84b469f
Elton <eltonbongue@gmail.com>
2025-08-21 11:18:36 +0100

    create ProdutoRepository file

--- app/src/main/java/com/elton/xdordersprototipojetpackcompose/Repository/ProdutoRepository.kt
+++ app/src/main/java/com/elton/xdordersprototipojetpackcompose/Repository/ProdutoRepository.kt
@@ -1,4 +1,23 @@
 package com.elton.xdordersprototipojetpackcompose.Repository
 
-class ProdutoRepository {
+import com.elton.xdordersprototipojetpackcompose.data.local.DAO.ProdutoDao
+import com.elton.xdordersprototipojetpackcompose.domain.model.Product
+import com.elton.xdordersprototipojetpackcompose.domain.model.ProdutoCompleto
+import javax.inject.Inject
+
+class ProdutoRepository @Inject constructor(
+    private val dao: ProdutoDao
+) {
+    suspend fun getProdutosPorCategoria(categoriaId: Int): List<Product> {
+        return dao.getProdutosPorCategoria(categoriaId)
+    }
+
+    suspend fun getTodosProdutos(): List<Product> {
+        return dao.getTodosProdutos()
+    }
+
+    suspend fun getProdutoCompletoPorId(id: Int): ProdutoCompleto? {
+        return dao.getProdutoCompletoPorId(id)
+    }
 }
+
